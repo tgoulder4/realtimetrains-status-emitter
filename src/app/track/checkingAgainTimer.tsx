6a217ceaded21fe8e62f924aadeb343aa0c7fd85
Tye <tcjgoulder@gmail.com>
2024-08-12 14:04:01 +0100

    fix odd time remaining behaviour

--- src/app/track/checkingAgainTimer.tsx
+++ src/app/track/checkingAgainTimer.tsx
@@ -7,8 +7,11 @@ type Props = {
 
 function CheckingAgainTimer({ startTime }: Props) {
     const [timeRemaining, setTimeRemaining] = useState(startTime)
-    console.log("timeRemaining: ", timeRemaining)
+    if (timeRemaining > startTime) setTimeRemaining(startTime)
     useEffect(() => {
+        console.log("timeRemaining: ", timeRemaining)
+        console.log("startTime: ", startTime)
+        console.log("timeRemaining / startTime: ", timeRemaining / startTime)
         const interval = setInterval(() => {
             setTimeRemaining(timeRemaining - 1000)
         }, 1000)
@@ -16,13 +19,13 @@ function CheckingAgainTimer({ startTime }: Props) {
             clearInterval(interval)
         }
         return () => clearInterval(interval)
-    }, [timeRemaining])
+    }, [timeRemaining, startTime]);
     return (
         <div className="w-36 refreshInTimer pb-0 bg-white">
-            <div className="grid px-12 place-items-center h-[calc(100%-12px)]">
+            <div className="grid place-items-center h-[calc(100%-12px)]">
                 <Loader2 size={32} className={`transtion-all animate ${timeRemaining <= 0 ? "animate-reverse-spin text-black/90" : "animate-spin text-black/50"}`} />
             </div>
-            <motion.div id="progressBar" className='h-3 bg-black transition-all duration-700' style={{ width: `calc(${(timeRemaining) / startTime} * 100%)` }}></motion.div>
+            <motion.div id="progressBar" className='h-3 bg-black transition-all duration-700' style={{ width: `${(timeRemaining / startTime) * 100}%` }}></motion.div>
         </div>
     )
 }
