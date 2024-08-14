24019036aecc84f781cc852b68e15f556319ac67
Tye <tcjgoulder@gmail.com>
2024-08-14 07:29:44 +0100

    URGENT- rate limit by key

--- src/app/track/actions.tsx
+++ src/app/track/actions.tsx
@@ -1,3 +1,4 @@
+'use server'
 import { JourneySchema, TrackStateSchema } from "@/lib/schemas";
 import { TrackState } from "@/lib/types";
 import { unauthenticatedAction } from "@/lib/safe-action";
@@ -5,12 +6,14 @@ import { z } from "zod";
 import { getServiceListCA } from "@/core-actions/main";
 import { dissectOneTrainInfoFromUrl, dissectTrainInfoFromUrl } from "./dissectServicesToTrack";
 import { getTrackStateCA } from "@/core-actions/track";
+import { rateLimitByKey } from "@/lib/limiter";
 
 export const getTrackStateSA = unauthenticatedAction
     .createServerAction()
     .input(z.object({ journey: JourneySchema }))
     .handler(async ({ input }) => {
         // console.log("getTrackState called with input: ", input)
+        await rateLimitByKey({ key: "getTrackState", window: 10000, limit: 1 });
         const ts = await getTrackStateCA(input.journey);
         // console.log("getTrackState returning: ", ts);
         return ts as TrackState;
