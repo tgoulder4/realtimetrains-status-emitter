c76d6ef01c03bfe90cc4c2680c5550ff346737ca
Tye <tcjgoulder@gmail.com>
2024-08-14 08:21:44 +0100

    fetch alternative ways

--- src/env.ts
+++ src/env.ts
@@ -4,11 +4,13 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         NODE_ENV: z.string().optional(),
+        SCRAPEOPS_API_KEY: z.string(),
         // HOST_NAME: z.string().min(1),
     },
     client: {},
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
+        SCRAPEOPS_API_KEY: process.env.SCRAPEOPS_API_KEY,
         // HOST_NAME: process.env.HOST_NAME
     },
 });
