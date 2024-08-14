a3f00f7753032b788f4cd717b61eb4dee0f7fa43
Tye <tcjgoulder@gmail.com>
2024-08-14 03:22:49 +0100

    status aware timer

--- src/app/track/checkingAgainTimer.tsx
+++ src/app/track/checkingAgainTimer.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 import { Loader2 } from 'lucide-react'
+import { changeColour } from '@/lib/colours'
+import { getColourFromStatus, getHexColourFromStatus } from './getRenderInfoFromState'
+import { Service } from '@/lib/types'
 type Props = {
     startTime: number,
-    updateKey: string
+    updateKey: string,
+    status: Service['status']
 }
 
-function CheckingAgainTimer({ startTime, updateKey }: Props) {
+function CheckingAgainTimer({ startTime, status, updateKey }: Props) {
     const timeRemainingRef = useRef(startTime);
     const [timeToRender, setTimeToRender] = useState(timeRemainingRef.current);
     const intervalRef = useRef<NodeJS.Timeout>();
@@ -22,13 +26,13 @@ function CheckingAgainTimer({ startTime, updateKey }: Props) {
         return () => clearInterval(intervalRef.current)
     }, [startTime, updateKey]);
     return (
-        <div className="w-36 refreshInTimer pb-0 bg-white">
-            <div className="grid place-items-center h-[calc(100%-12px)]">
-                <Loader2 size={32} className={`transtion-all animate ${timeToRender <= 1 ? "duration-700 text-black" : " text-black/20"} animate-spin`} />
+        <div className="flex-[2] w-full h-auto refreshInTimer pb-0 bg-white/40">
+            <div className="grid place-items-center h-full">
+                <Loader2 size={32} className={`transtion-all animate duration-1000 ${timeToRender <= 1 ? "duration-700 text-black" : " text-black/20"} animate-spin`} />
             </div>
-            <motion.div id="progressBar" className='h-3 bg-black transition-all duration-700' style={{
+            <motion.div id="progressBar" className='-mt-3 h-3  transition-all duration-700' style={{
                 width: `${(timeToRender / startTime) * 100}%`,
-                backgroundColor: timeToRender <= 0 ? "red" : `rgba(0,0,0,${(timeToRender / startTime)})`
+                backgroundColor: timeToRender <= 0 ? "red" : `${changeColour(getHexColourFromStatus(status)).setAlpha(timeToRender / startTime)}`
             }}>
                 {timeToRender <= -1 ? "Connection lost" : ""}
             </motion.div>
