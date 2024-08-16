77eac30558da49ee67d7a4210cd591a7c55b74c7
Tye <tcjgoulder@gmail.com>
2024-08-16 02:05:54 +0100

    logo brings to /find

--- src/app/track/LogoWithName.tsx
+++ src/app/track/LogoWithName.tsx
@@ -7,8 +7,10 @@ type Props = {
 function HeaderLogoWithName({ pageTitle }: Props) {
     return (
         <div className='flex flex-col items-center gap-4'>
-            <svg color='white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"
-                className="opacity-70 lucide lucide-train-front"><path d="M8 3.1V7a4 4 0 0 0 8 0V3.1" /><path d="m9 15-1-1" /><path d="m15 15 1-1" /><path d="M9 19c-2.8 0-5-2.2-5-5v-4a8 8 0 0 1 16 0v4c0 2.8-2.2 5-5 5Z" /><path d="m8 19-2 3" /><path d="m16 19 2 3" /></svg>
+            <a href="/find">
+                <svg color='white' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"
+                    className="opacity-70 lucide lucide-train-front"><path d="M8 3.1V7a4 4 0 0 0 8 0V3.1" /><path d="m9 15-1-1" /><path d="m15 15 1-1" /><path d="M9 19c-2.8 0-5-2.2-5-5v-4a8 8 0 0 1 16 0v4c0 2.8-2.2 5-5 5Z" /><path d="m8 19-2 3" /><path d="m16 19 2 3" /></svg>
+            </a>
             <h2 className="font-semibold text-white text-2xl">{pageTitle}</h2>
         </div>
     )
