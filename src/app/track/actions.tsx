e230c5cb97c9eeb5458976963b495a76e9eb5cf7
Tye <tcjgoulder@gmail.com>
2024-08-16 02:14:31 +0100

    redirect before pass to client

--- src/app/track/actions.tsx
+++ src/app/track/actions.tsx
@@ -7,6 +7,7 @@ import { getServiceListCA } from "@/core-actions/main";
 import { dissectOneTrainInfoFromUrl, dissectTrainInfoFromUrl } from "./dissectServicesToTrack";
 import { getTrackStateCA } from "@/core-actions/track";
 import { rateLimitByKey } from "@/lib/limiter";
+import { redirect } from "next/navigation";
 
 export const getTrackStateSA = unauthenticatedAction
     .createServerAction()
@@ -15,6 +16,7 @@ export const getTrackStateSA = unauthenticatedAction
         // console.log("getTrackState called with input: ", input)
         await rateLimitByKey({ key: "getTrackState", window: 10000, limit: 10 });
         const ts = await getTrackStateCA(input.journey);
+        if (ts.hidden.error) redirect('/find?err=' + ts.hidden.error);
         // console.log("getTrackState returning: ", ts);
         return ts as TrackState;
     })
