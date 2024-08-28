c58ba28df9b8765c5dd451a5bceced8bc94e062b
Tye <tcjgoulder@gmail.com>
2024-08-28 06:16:22 +0100

    copyright tye

--- src/app/track/LogoWithName.tsx
+++ src/app/track/LogoWithName.tsx
@@ -12,6 +12,7 @@ function HeaderLogoWithName({ pageTitle }: Props) {
                     className="opacity-70 lucide lucide-train-front"><path d="M8 3.1V7a4 4 0 0 0 8 0V3.1" /><path d="m9 15-1-1" /><path d="m15 15 1-1" /><path d="M9 19c-2.8 0-5-2.2-5-5v-4a8 8 0 0 1 16 0v4c0 2.8-2.2 5-5 5Z" /><path d="m8 19-2 3" /><path d="m16 19 2 3" /></svg>
             </a>
             <h2 className="font-semibold text-white text-2xl">{pageTitle}</h2>
+            <a href={`https://buildwithtye.com/portfolio${localStorage ? localStorage.getItem("sessionID") ? "?session_id=" + localStorage.getItem("sessionID") : "" : ""}`} className='text-white opacity-20 font-medium -mt-2 w-full text-center'>© tye goulder</a>
         </div>
     )
 }
